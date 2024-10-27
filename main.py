e51cf8459935f4c08c80ed99140814c203c1801d
phambao <phambaose@gmail.com>
2024-10-27 19:34:30 +0700

    firest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
 import time
 import csv
 from crewai import Crew
-from langchain_groq import ChatGroq
 from agents import EmailPersonalizationAgents
 from tasks import PersonalizeEmailTask
 
@@ -10,45 +8,48 @@ from tasks import PersonalizeEmailTask
 from dotenv import load_dotenv
 load_dotenv()
 
-email_template = """
-Hey [Name]!
+def generate_email(email_template, csv_file):
 
-Just a quick reminder that we have a Skool community where you can 
-join us for weekly coaching calls every Tuesday at 6 PM Eastern time.
-The community is completely free and we're about to hit the 500
-user milestone. We'd love to have you join us!
 
-If you have any questions or need help with your projects, 
-this is a great place to connect with others and get support. 
+    # email_template = """
+    # Hey [Name]!
 
-If you're enjoying the AI-related content, make sure to check out 
-some of the other videos on my channel. Don't forget to hit that 
-like and subscribe button to stay updated with the latest content. 
-Looking forward to seeing you in the community!
+    # Just a quick reminder that we have a Skool community where you can 
+    # join us for weekly coaching calls every Tuesday at 6 PM Eastern time.
+    # The community is completely free and we're about to hit the 500
+    # user milestone. We'd love to have you join us!
 
-Best regards,
-Brandon Hancock
-"""
+    # If you have any questions or need help with your projects, 
+    # this is a great place to connect with others and get support. 
 
-# 1. Create agents
-agents = EmailPersonalizationAgents()
+    # If you're enjoying the AI-related content, make sure to check out 
+    # some of the other videos on my channel. Don't forget to hit that 
+    # like and subscribe button to stay updated with the latest content. 
+    # Looking forward to seeing you in the community!
 
-email_personalizer = agents.personalize_email_agent()
-ghostwriter = agents.ghostwriter_agent()
+    # Best regards,
+    # Brandon Hancock
+    # """
 
-# 2. Create tasks
-tasks = PersonalizeEmailTask()
+    # 1. Create agents
+    agents = EmailPersonalizationAgents()
 
-personalize_email_tasks = []
-ghostwrite_email_tasks = []
+    email_personalizer = agents.personalize_email_agent()
+    ghostwriter = agents.ghostwriter_agent()
 
-# Path to the CSV file containing client information
-csv_file_path = 'data/clients_small.csv'
+    # 2. Create tasks
+    tasks = PersonalizeEmailTask()
 
-# Open the CSV file
-with open(csv_file_path, mode='r', newline='') as file:
-    # Create a CSV reader object
-    csv_reader = csv.DictReader(file)
+    personalize_email_tasks = []
+    ghostwrite_email_tasks = []
+
+    # Path to the CSV file containing client information
+    csv_file_path = 'data/clients_small.csv'
+
+    # Open the CSV file
+    # with open(csv_file_path, mode='r', newline='') as file:
+        # Create a CSV reader object
+    csv_reader = csv.DictReader(csv_file)
 
     # Iterate over each row in the CSV file
     for row in csv_reader:
@@ -80,26 +81,28 @@ with open(csv_file_path, mode='r', newline='') as file:
         ghostwrite_email_tasks.append(ghostwrite_email_task)
 
 
-# Setup Crew
-crew = Crew(
-    agents=[
-        email_personalizer,
-        ghostwriter
-    ],
-    tasks=[
-        *personalize_email_tasks,
-        *ghostwrite_email_tasks
-    ],
-    max_rpm=29
-)
+    # Setup Crew
+    crew = Crew(
+        agents=[
+            email_personalizer,
+            ghostwriter
+        ],
+        tasks=[
+            *personalize_email_tasks,
+            *ghostwrite_email_tasks
+        ],
+        max_rpm=29
+    )
 
-# Kick off the crew
-start_time = time.time()
+    # Kick off the crew
+    start_time = time.time()
 
-results = crew.kickoff()
+    results = crew.kickoff()
 
-end_time = time.time()
-elapsed_time = end_time - start_time
+    end_time = time.time()
+    elapsed_time = end_time - start_time
 
-print(f"Crew kickoff took {elapsed_time} seconds.")
-print("Crew usage", crew.usage_metrics)
+    print(f"Crew kickoff took {elapsed_time} seconds.")
+    print("Crew usage", crew.usage_metrics)
+
+    return results
